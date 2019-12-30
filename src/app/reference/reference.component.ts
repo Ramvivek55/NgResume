af59c7ed95f4b65b1ae72a33cc955eb6220ae1cf
ramvivek <ramvivek.r@talpeo.com>
2019-12-30 05:41:39 -0800

    full pane done

--- src/app/reference/reference.component.ts
+++ src/app/reference/reference.component.ts
@@ -1,29 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 
 declare var $: any;
 @Component({
   selector: 'app-reference',
   templateUrl: './reference.component.html',
-  styleUrls: ['./reference.component.css']
+  // styleUrls: ['./reference.component.css']
+  styles: [`
+    .mat-dialog-container {
+      padding: 0px !important;
+      box-shadow: none !important;
+    }
+    .cdkoverlay{
+      display: none;
+    }
+    .cdks{
+      opacity: 0;
+    }
+    .sidelines{
+      position: absolute;
+    }
+    
+  `],
+  encapsulation: ViewEncapsulation.None
 })
 export class ReferenceComponent implements OnInit {
 
+  isHovering: boolean;
+
   constructor(public dialog: MatDialog) { }
 
-  openDialog(): void {
-    const dialogRef = this.dialog.open(DialogModelComponent, {
-      width: '400px',
-    });
+  openDialog($event): void {
+    const dialogRef = this.dialog.open(DialogModelComponent) 
   }
 
 
   ngOnInit() {
   }
 
+
   ngAfterViewInit() {
     
-    $('#pass1').click(function () {
+    $('#pass1').mouseover(function () {
       $('#fail1').removeClass('a1');
       $('#fail2').addClass('a2');
       $('#fail3').addClass('a3');
@@ -31,7 +49,7 @@ export class ReferenceComponent implements OnInit {
       $('#fail5').addClass('a5');
     });
 
-    $('#pass2').click(function () {
+    $('#pass2').mouseover(function () {
       $('#fail1').addClass('a1');
       $('#fail2').removeClass('a2');
       $('#fail3').addClass('a3');
@@ -39,7 +57,7 @@ export class ReferenceComponent implements OnInit {
       $('#fail5').addClass('a5');
     });
 
-    $('#pass3').click(function () {
+    $('#pass3').mouseover(function () {
       $('#fail1').addClass('a1');
       $('#fail2').addClass('a2');
       $('#fail3').removeClass('a3');
@@ -47,7 +65,7 @@ export class ReferenceComponent implements OnInit {
       $('#fail5').addClass('a5');
     });
 
-    $('#pass4').click(function () {
+    $('#pass4').mouseover(function () {
       $('#fail1').addClass('a1');
       $('#fail2').addClass('a2');
       $('#fail3').addClass('a3');
@@ -55,7 +73,7 @@ export class ReferenceComponent implements OnInit {
       $('#fail5').addClass('a5');
     });
 
-    $('#pass5').click(function () {
+    $('#pass5').mouseover(function () {
       $('#fail1').addClass('a1');
       $('#fail2').addClass('a2');
       $('#fail3').addClass('a3');
@@ -63,6 +81,16 @@ export class ReferenceComponent implements OnInit {
       $('#fail5').removeClass('a5');
     });
 
+  }
+
+
+
+  changeText1(){
+  this.isHovering =  true;
+  }
+
+  changeText2(){
+    this.isHovering =  false;
   }
 
 }
@@ -73,8 +101,10 @@ export class ReferenceComponent implements OnInit {
   templateUrl: './dialog-model.component.html',
   styleUrls: ['./dialog-model.component.css']
 })
+
 export class DialogModelComponent {
 
+  
   constructor(public dialogRef: MatDialogRef<DialogModelComponent>) { }
 
   onNoClick(): void {
--- src/assets/js/option-panel.js
+++ src/assets/js/option-panel.js
@@ -83,7 +83,7 @@
 	
 	function createOptionPanel(){
 		var $html = '<div id="op-panel">'+
-			'<div id="op-panel-btn" style="height: 34px!important;"><i  class="fa fa-cog fa-2x" style="padding-left: 8px;" aria-hidden="true"></i></div>'+
+			'<div id="op-panel-btn" style="height: 34px!important;width: 0rem;right: 0rem;"><i  class="fa fa-cog fa-2x" style="padding-left: 8px;" aria-hidden="true"></i></div>'+
 			'<div class="op-content">'+
                 '<div class="op-section">'+
                     '<div class="op-theme-skin op-btn-group">'+
