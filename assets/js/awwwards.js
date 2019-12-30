f67bae0f1360ad6d6d05898ad5afa417e60b8178
ramvivek <ramvivek.r@talpeo.com>
2019-12-30 05:56:20 -0800

    Auto-generated commit

--- assets/js/awwwards.js
+++ assets/js/awwwards.js
@@ -2,6 +2,6 @@
     "use strict";
 	
 	$(function() {
-		$('body').append('<div id="awwwards" class="nominee white right"><a href="http://www.awwwards.com/best-websites/material-design-resume-cv-portfolio/" target="_blank">Awwwards</a></div>');
+		$('body').append('<div id="awwwards" class="nominee white right"></div>');
 	});
 })(jQuery);
